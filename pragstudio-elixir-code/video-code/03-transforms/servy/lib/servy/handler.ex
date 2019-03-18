4d24fce740b9d2fd91f3f5a7a72698e7d5166982
dillonius01 <dillon.c.powers@gmail.com>
2019-03-18 13:21:11 -0400

    adds pragstudio stuff

---
+++ pragstudio-elixir-code/video-code/03-transforms/servy/lib/servy/handler.ex
@@ -0,0 +1,43 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    request
+    |> parse
+    |> log
+    |> route
+    |> format_response
+  end
+
+  def log(conv), do: IO.inspect(conv)
+
+  def parse(request) do
+    # TODO: Parse the request string into a map:
+    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def route(conv) do
+    # TODO: Create a new map that also has the response body:
+    %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{byte_size(conv.response_body)}
+
+    #{conv.response_body}
+    """
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
